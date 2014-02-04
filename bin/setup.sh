6ef8cde821cb3604c1e93f2e4ec201565b49bd18
Carl Sutherland <carl@humblesoftware.com>
2014-02-03 21:03:50 -0800

    Add Spotify to setup.

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,3 +2,10 @@
 
 # Setup ubuntu sytems
 sudo apt-get install aptitude git ack-grep vim tmux firefox chromium-browser
+
+# Spotify:
+echo '# Spotify:' >> /etc/apt/sources.list
+echo 'deb http://repository.spotify.com stable non-free' >> /etc/apt/sources.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59;
+sudo apt-get update;
+sudo apt-get install spotify-client;
