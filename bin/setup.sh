16c8c2c29166800b4384a663cc33e93b03b0423f
Carl Sutherland <carl@humblesoftware.com>
2014-03-17 12:31:00 -0700

    Add redshift to setup.

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Setup ubuntu sytems
-sudo apt-get install aptitude git ack-grep vim tmux firefox chromium-browser virtualbox keepass2 ssh curl g++
+sudo apt-get install aptitude git ack-grep vim tmux firefox chromium-browser virtualbox keepass2 ssh curl g++ redshift
 
 # Spotify:
 sudo add-apt-repository "deb http://repository.spotify.com stable non-free"
