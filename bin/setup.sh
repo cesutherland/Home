c98c0cbc8e1a8c5b4f43e259b5692a20d795786c
Carl Sutherland <carl@humblesoftware.com>
2016-08-20 10:59:01 -0700

    Update setup file.

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
 # Setup ubuntu sytems
-sudo apt-get install aptitude git ack-grep vim tmux firefox chromium-browser virtualbox keepass2 ssh curl g++ redshift libnotify-bin sqlite postgresql
+sudo add-apt-repository multiverse
+sudo apt-get install aptitude git ack-grep vim tmux firefox chromium-browser virtualbox keepass2 ssh curl g++ redshift libnotify-bin sqlite postgresql steam vlc mplayer mpv
 
 # Spotify:
-sudo add-apt-repository "deb http://repository.spotify.com stable non-free"
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59;
-sudo apt-get update;
-sudo apt-get install spotify-client;
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update
+sudo apt-get install spotify-client
+
+# Chrome:
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt-get update
+sudo apt-get install google-chrome-stable
